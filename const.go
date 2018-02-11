646288d9ee0bd44d4b7ddbb0f3631558730beae1
Tsuzu <tsuzu.0623@gmail.com>
2018-02-11 18:30:00 +0900

    Update version

--- const.go
+++ const.go
@@ -4,8 +4,9 @@ import "github.com/gorilla/websocket"
 
 const (
 	Version1_0 = "1.0"
+	Version1_1 = "1.1" // Update following go-easyp2p
 
-	VersionLatest = Version1_0
+	VersionLatest = Version1_1
 )
 
 var upgrader = websocket.Upgrader{
