b2aefb4a050603eb4276fd73621a88cda8c6bd41
Tsuzu <tsuzu.0623@gmail.com>
2018-02-11 19:16:47 +0900

    Add make_release.sh

---
+++ make_release.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -eu
+
+GOOS=linux go build -o ftrans *.go
+GOOS=darwin go build -o ftrans_mac *.go
+GOOS=windows go build -o ftrans.exe *.go
+
+FTRANS=$(openssl md5 ftrans)
+FTRANS_MAC=$(openssl md5 ftrans_mac)
+FTRANS_WIN=$(openssl md5 ftrans.exe)
+
+cat << EOF
+# Version $1
+
+The all following binaries are ones for x86_64.
+
+# Linux
+[Download binary]()
+$FTRANS
+
+# Mac
+[Download binary]()
+$FTRANS_MAC
+
+# Windows
+[Download binary]()
+$FTRANS_WIN
+
+EOF
+
