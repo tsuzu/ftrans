7899c99535c068de66f0629eca726e92ca1cf261
Tsuzu <tsuzu.0623@gmail.com>
2018-02-18 12:42:32 +0900

    Fix terrible typo

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	mode      = flag.String("mode", "client", "Server(signaling server) or client(sender or reciever)")
+	mode      = flag.String("mode", "client", "Server(signaling server) or client(sender or receiver)")
 	stun      = flag.String("stun", "stun.l.google.com:19302", "STUN server addresses(split with ',')")
 	signaling = flag.String("sig", "wss://ftrans.cs3238.com/ws", "Signaling server address")
 	version   = flag.Bool("v", false, "Show version")
@@ -19,7 +19,7 @@ func main() {
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "Usage of ftrans:")
 		fmt.Fprintln(os.Stderr, "  ftrans [options] password [file paths...]")
-		fmt.Fprintln(os.Stderr, "  If no path is passed, this runs as a reciever.")
+		fmt.Fprintln(os.Stderr, "  If no path is passed, this runs as a receiver.")
 		fmt.Fprintln(os.Stderr)
 		fmt.Fprintln(os.Stderr, "  To launch a signaling server: ftrans --mode server")
 		fmt.Fprintln(os.Stderr)
